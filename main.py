36e1897cbd9931cce1c87d58c6a8e30d467fc5d0
platz-halter <simon.pressel2@proton.me>
2024-10-16 23:34:44 +0200

    Added first example function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def init_params(layer_dims):
+    np.random.seed(3)
+    params = {}
+    L = len(layer_dims)
+
+    for l in range(1, L):
+        params['W'+str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])*0.01
+        params['b'+str(l)] = np.zeros((layer_dims[l], 1))
+
+    return params
+
